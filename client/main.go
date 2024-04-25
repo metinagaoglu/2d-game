f8894b8ff2a131e73925c4fadb3dc6bb3e0644bb
Metin Ağaoğlu <metnagaoglu@gmail.com>
2024-04-25 18:28:55 +0300

    Implement moving to game oject using key handlers and Update method

--- client/main.go
+++ client/main.go
@@ -1,16 +1,56 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
-import . "assets"
+import (
+	. "assets"
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
-type Game struct{}
+type Vector struct {
+	X float64
+	Y float64
+}
+
+type Game struct {
+	playerPosition Vector
+}
 
 func (g *Game) Update() error {
+	speed := 5.0
+
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		g.playerPosition.X -= speed
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		g.playerPosition.X += speed
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		g.playerPosition.Y -= speed
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		g.playerPosition.Y += speed
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		return fmt.Errorf("Game closed")
+	}
+
+	// Handle sapce for debug
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		fmt.Println("Space pressed")
+		fmt.Println(g.playerPosition)
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.DrawImage(PlayerSprite, nil)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(g.playerPosition.X, g.playerPosition.Y)
+	screen.DrawImage(PlayerSprite, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -18,10 +58,13 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	g := &Game{}
+	g := &Game{
+		playerPosition: Vector{X: 100, Y: 300},
+	}
 
 	err := ebiten.RunGame(g)
 	if err != nil {
 		panic(err)
 	}
+
 }
